45cb2e3d3e6c909d04114490bed57be37e19a506
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-07-13 13:46:08 +0530

    Add : firebase / material / login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'codesdot-ebea9',
+    appId: '1:591514622855:web:ddad44857d2772a9df9122',
+    storageBucket: 'codesdot-ebea9.appspot.com',
+    apiKey: 'AIzaSyBZd3h4hSck3va_6YlP60YXl6ZDE1DxnHY',
+    authDomain: 'codesdot-ebea9.firebaseapp.com',
+    messagingSenderId: '591514622855',
+  },
   production: false
 };
 
